2febaf7d432524fb69644324595db5fb5656ae90
yoshiocamp <yoshio14393974@gmail.com>
2024-06-20 17:08:12 +0900

    createアクションの設定

--- app/controllers/kotobas_controller.rb
+++ app/controllers/kotobas_controller.rb
@@ -7,4 +7,14 @@ class KotobasController < ApplicationController
   def new
     @kotoba = Kotoba.new
   end
+
+  def create
+    Kotoba.create(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:kotoba).permit(:name, :text)
+  end
 end
