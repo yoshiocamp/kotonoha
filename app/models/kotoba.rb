e66daf9a51b6560fd87417f7ab2a2ef67cd1446c
yoshiocamp <yoshio14393974@gmail.com>
2024-06-20 17:38:55 +0900

    バリテーション追加

--- app/models/kotoba.rb
+++ app/models/kotoba.rb
@@ -1,3 +1,14 @@
 class Kotoba < ApplicationRecord
   validates :text, presence: true
+
+  validate :check_forbidden_words
+  
+    private
+  
+    def check_forbidden_words
+      forbidden_words = ["死ね", "殺す", "殺し"]
+      if forbidden_words.any? { |word| text.include?(word) }
+        errors.add(:text, "使用できない言葉が含まれています")
+      end
+    end
 end
