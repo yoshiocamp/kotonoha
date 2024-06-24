ce27cf9e6f99b21ecbacd11bfabbc8b947dde172
yoshiocamp <yoshio14393974@gmail.com>
2024-06-24 17:48:27 +0900

    viewの整頓、字の見せ方

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+    def truncate_with_newline(text, length: 45, omission: '...')
+      if text.include?("\n")
+        first_line = text.split("\n").first
+      truncated_first_line = first_line.truncate(length, omission: omission)
+      truncated_first_line += "..." if first_line.length > length || text.split("\n").size > 1
+      truncated_first_line
+      else
+        text.truncate(length, omission: omission)
+      end
+    end
+  end
